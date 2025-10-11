b98aabccd1a9effadd6e25339ae5d0faadb7b788
AbhishekDhallania <dhallaniaabhishek@gmail.com>
2025-10-11 22:17:37 +0530

    Fix backend with CORS and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fetch from "node-fetch";
 import dotenv from "dotenv";
+import cors from "cors"
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -12,10 +13,9 @@ const PORT = 5000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Serve static files (frontend)
+app.use(cors)
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Proxy route to fetch weather
 app.get("/api/weather", async (req, res) => {
   const city = req.query.city;
   if (!city) return res.status(400).json({ error: "City required" });
